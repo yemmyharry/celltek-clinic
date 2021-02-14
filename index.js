7b21dcdc18bea3edb4bcfef5ff9c319c61aa01fe
yemmyharry <yemmyharry@gmail.com>
2021-02-14 18:15:32 +0100

    added port env

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ app.delete('/post/:id', async (req, res) => {
 })
 
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("App listening on port 4000");
 });                         
