28c63c132d6591673ad1609e210ba84d9d6f5b59
yemmyharry <yemmyharry@gmail.com>
2021-02-18 09:40:33 +0100

    redirect to https

--- index.js
+++ index.js
@@ -41,10 +41,23 @@ const mongoStore = connectMongo(expressSession);
  
 app.use(expressSession({
     secret: 'secret',
+    httpOnly: true,
+    secure: true,
     store: new mongoStore({
         mongooseConnection: mongoose.connection
     })
 }));
+
+
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 app.use(methodOverride("_method"))
 app.use(connectFlash());
