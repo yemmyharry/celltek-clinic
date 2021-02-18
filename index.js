45bf29d09e29e9f99a36d74dbdbce53ff942e438
yemmyharry <yemmyharry@gmail.com>
2021-02-18 10:20:38 +0100

    changed redirect syntax for https

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(expressSession({
 }));
 
 
-app.use (function (req, res, next) {
+app.all(function (req, res, next) {
   if (req.secure) {
           // request was via https, so do no special handling
           next();
